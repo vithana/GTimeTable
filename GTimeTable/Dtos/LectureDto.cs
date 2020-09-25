c4c6c013cf3d73c05e49b34932adc119a2c3a195
vithana <sajeewa998@gmail.com>
2020-09-25 19:26:04 +0530

    Lecturers Completed

---
+++ GTimeTable/Dtos/LectureDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GTimeTable.Dtos
+{
+    public class LectureDto
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string emp_id { get; set; }
+        public string faculty { get; set; }
+        public string dept { get; set; }
+        public string center { get; set; }
+        public string building { get; set; }
+        public int lvl { get; set; }
+        public string rank { get; set; }
+
+       
+    }
+}
