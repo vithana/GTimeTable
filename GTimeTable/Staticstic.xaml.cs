55373011b3e260b47105cd415e2d30fe1b75ea0c
dasunpriyankara <dasundurage@gmail.com>
2020-10-02 23:26:54 +0530

    Dashboard UI

--- GTimeTable/Staticstic.xaml.cs
+++ GTimeTable/Staticstic.xaml.cs
@@ -20,9 +20,100 @@ namespace GTimeTable
     /// </summary>
     public partial class Staticstic : UserControl
     {
+
+        GTimeTableEntities _db = new GTimeTableEntities();
+
+        //------- Add  Buildng  Tab-----------------
+        int buildingId;
+        int roomId;
+        Building building = new Building();
+
+        //------- Add  Rooms  Tab-----------------
+        Room room;
+
+        //-------- Suitable Rooms for tab -------------
+        SutiableTabforRoom suitableRoomsforTag;
+
+
         public Staticstic()
         {
             InitializeComponent();
+            lecturerCount();
+        }
+
+        private void lecturerCount()
+        {
+            List<Building> buildings = new List<Building>();
+            buildings = _db.Buildings.ToList();
+
+            List<Room> rooms = new List<Room>();
+            rooms = _db.Rooms.ToList();
+
+            List<Subject> subjects = new List<Subject>();
+            subjects = _db.Subjects.ToList();
+
+            List<Session> sessions = new List<Session>();
+            sessions = _db.Sessions.ToList();
+
+            List<Student> groups = new List<Student>();
+            groups = _db.Students.ToList();
+
+        
+
+
+
+            var lecCount =0;
+            var subCount = 0;
+            var roomCount = 0;
+            var sessCount = 0;
+            var groupCount = 0;
+            var buildingCount = 0;
+
+
+            foreach (var building in buildings)
+            {
+                lecCount++;
+            }
+
+        
+
+            foreach (var room in rooms)
+            {
+                roomCount++;
+            }
+
+          
+
+            foreach (var subject in subjects)
+            {
+                subCount++;
+            }
+
+            foreach (var session in sessions)
+            {
+                sessCount++;
+            }
+
+            foreach (var building in buildings)
+            {
+                buildingCount++;
+            }
+
+            foreach (var group in groups)
+            {
+                groupCount++;
+            }
+
+
+
+            numOfLecTxtBx.Text = lecCount.ToString();
+            numOfSubTxtBx.Text = subCount.ToString();
+            numOfRoomsTxtBx.Text = roomCount.ToString();
+            numOfSessionTxtBx.Text = sessCount.ToString();
+            numOfBuildingTxtBx.Text = buildingCount.ToString();
+            numOfGroupsTxtBx.Text = groupCount.ToString();
+
+
         }
 
         private void textBox_TextChanged(object sender, TextChangedEventArgs e)
